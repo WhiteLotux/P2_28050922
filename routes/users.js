e2561e5807fa64fcc927fb2be0d4a4f5006eb32f
WhiteLotux <129133826+WhiteLotux@users.noreply.github.com>
2023-05-13 13:22:02 -0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.post('/', function(req, res, next)){
+  let name = req.body.name;
+  let email = req.body.email;
+  let comment = req.body.comment;
+  let date = new Date();
+  let ip = req.headers['x-forwarde-for] || req.socket.remoteAddress;
+                       
+    db.insert(name, email, comment, date, ip);
+
+console.log({name, email, comment, date, ip});
+  res.redirect('/')
+});
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
