ea48cb6047cb349934a4daee5e293aab7e5cfc5d
WhiteLotux <129133826+WhiteLotux@users.noreply.github.com>
2023-06-17 15:30:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const app = express();
-const request = require ('request');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-require('dotenv').config();
-const bcrypt = require('bcrypt');
 
-
-app.set('view engine', 'ejs');
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,11 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/contacto', usersRouter);
-
-app.get('/views/cv.html', function(req, res){
-  res.sendFile (__dirname + '/views/cv.html')
-})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,4 +38,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app; 
+module.exports = app;
