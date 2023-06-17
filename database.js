a00240ad9f1b7e38cbbccc51b7d3ded77975b3e0
WhiteLotux <129133826+WhiteLotux@users.noreply.github.com>
2023-06-17 10:50:33 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,18 +1,18 @@
 const sqlite3 = require('sqlite3').verbose();
 
-
-let db = new sqlite3.Database(':index.db',(err) => {
+let db = new sqlite3.Database(':memory:', (err) => {
     if (err) {
         return console.error(err.message);
     }
     console.log('Connected to the in-memory SQlite database.');
 
-    db.run("CREATE TABLE IF NOT EXISTS contactos (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL, cell TEXT NOT NULL, comment TEXT NOT NULL, date TEXT NOT NULL, ip TEXT NOT NULL, country TEXT NOT NULL)");
+    db.run("CREATE TABLE IF NOT EXISTS contactos (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL, comment TEXT NOT NULL, date TEXT NOT NULL, ip TEXT NOT NULL, loc TEXT NOT NULL)");
 });
 
+
 module.exports = {
-    insert: function (name, email, cell, comment, Datetime, myIP, country) {
-        db.run("INSERT INTO contactos (name, email, cell, comment, date, ip, country) VALUES (?, ?, ?, ?, ?, ?, ?)", [name, email, cell, comment, Datetime, myIP, country], function (err) {
+    insert: function (name, email, comment, date, ip, loc) {
+        db.run("INSERT INTO contactos (name, email, comment, date, ip, loc) VALUES (?, ?, ?, ?, ?, ?)", [name, email, comment, date, ip, loc], function (err) {
             if (err) {
                 return console.log(err.message);
             }
