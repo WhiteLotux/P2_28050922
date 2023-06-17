cb650a516d83114c9ef0e3f357c15a8cf43b8e8b
WhiteLotux <129133826+WhiteLotux@users.noreply.github.com>
2023-06-17 11:24:07 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,86 +1,63 @@
-let express = require('express');
-const db = require('../database');
-let router = express.Router();
-const request = require ('request');
-const IP = require ('ip');
-const nodemailer = require('nodemailer');
-require('dotenv').config();
-
-//Pagina de inicio 
-router.get('/', function(req, res, next) {
-  let name = 'Omar Pimentel'
-  res.render('index', {
-    title: 'Formulario de contacto',
-    name: name, });});
-
-
-//Ejecuta acciones del formulario
-router.post('/', function(req, res, next) {  
-  //Declaracion de variables locales
- 
-  let name = req.body.name;
-  let email = req.body.email;
-  let cell = req.body.cell;
+let email = req.body._replyto;
   let comment = req.body.comment;
-  let date = new Date();
-  let fecha = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-  let Datetime = fecha;
-  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  const myIP = ip.split(",")[0];
+  let date = new Date(); 
+  const clientIp = requestIp.getClientIp(req)
+  const ip = clientIp
+  let now = date.toLocaleString()
+  // let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // @todo falta formatear la ip
+  let loc
+   // replace with the IP address you want to look up
+  // const geo = geoip.lookup("190.37.91.206");
+    const geo = geoip.lookup(ip);
 
+  loc = (geo.country);
 
-  //Localizar pais de origen de la IP
-  request(`http://ip-api.com/json/${myIP}`, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-      const data = JSON.parse(body);
-      let country = data.country;
-      //Mostrar datos ingresados pos consola
-      console.log({name, email, cell, comment, Datetime, myIP, country});
-      //Insertar daton en la base de datos
-      db.insert(name, email, cell, comment, Datetime, myIP, country);
-      
-      
-      //Enviar email con los datos ingresados 
-      const transporter = nodemailer.createTransport({
-        host: process.env.hostemail,
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.useremail,
-            pass: process.env.passemail
-        }
-      });
-      const mailOptions = {
-        from: process.env.useremail,
-        //Lista de correos 
-        to: ['fuertegamer16@gmail.com', 'archpaladinsoul@gmail.com', 'programacion2ais@dispostable.com'],
-        subject: '* Task 3: Third Party Connection * ',
-        text: 'Un nuevo usuario se ha registrado en el formulario:\n' + 'Nombre: ' + name + '\nCorreo: ' + email + '\nTelefono: ' + cell + '\nMensaje: ' + comment + '\nFecha y hora: ' + Datetime + '\nIP: ' + myIP + '\nUbicacion: ' + country
-      };
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Correo electrónico enviado: ' + info.response);
-        }});}});
-        //Validacion de reCAPTCHA 
-        request(url, (err, response, body) => {
-          if (body.success && body.score) {
-            console.log('exitoso')
-          } else {
-            console.log('fracaso')
-          }
-        });
-    //Redirigir a pagina nuevamente
-    res.redirect('/');
+  db.insert(name, email, comment, now, ip, loc);
+  
+  //Crear Transportador de correo
+  var transporter = nodemailer.createTransport ({
+    
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    // service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  // Crear un objeto de opciones de correo
+  var mailOptions = {
+    nombre: name,
+    from: email,
+    to: process.env.EMAIL_SEND,
+    // to: "kelvinpaez2004@gmail.com",
+    subject: 'Contacto desde el formulario',
+    text: "Enviado por " + name + "\nEmail: " + email + "\nMensaje: " + comment + "\nIP: " + ip + "\nPais: " + loc
+  };
+
+  // Enviar el correo electrónico con el transportador
+  transporter.sendMail (mailOptions, function (error, info) {
+    // Manejar el error o la respuesta
+    if (error) {
+      console.log (error);
+      res.send ('Ocurrió un error al enviar el correo.');
+    } else {
+      console.log ('Correo enviado: ' + info.response);
+      res.send ('Correo enviado correctamente.');
+    }
+  });
+  res.redirect('/contact');
 });
+  
 
-//Insertar datos
-router.get('/contactos', function(req, res, next) {
+router.get('/contact', function(req, res, next) {
   db.select(function (rows) {
     console.log(rows);
   });
-  res.send('ok');
+  res.send('Se ha guardado la informacion del formularion en la base de datos y se envio el correo electronico');
 });
+
 
 module.exports = router;
